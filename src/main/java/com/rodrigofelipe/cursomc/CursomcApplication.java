a6fa35264c1b9dc41a9089bea0bb50455fcb2324
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-25 14:56:11 -0300

    Cliente. TipoCliente. telefones e endereços

--- src/main/java/com/rodrigofelipe/cursomc/CursomcApplication.java
+++ src/main/java/com/rodrigofelipe/cursomc/CursomcApplication.java
@@ -9,12 +9,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.rodrigofelipe.cursomc.domain.Categoria;
 import com.rodrigofelipe.cursomc.domain.Cidade;
+import com.rodrigofelipe.cursomc.domain.Cliente;
+import com.rodrigofelipe.cursomc.domain.Endereco;
 import com.rodrigofelipe.cursomc.domain.Estado;
 import com.rodrigofelipe.cursomc.domain.Produto;
+import com.rodrigofelipe.cursomc.domain.enums.TipoCliente;
 import com.rodrigofelipe.cursomc.repositories.CategoriaRepository;
 import com.rodrigofelipe.cursomc.repositories.CidadeRepository;
+import com.rodrigofelipe.cursomc.repositories.ClienteRepository;
+import com.rodrigofelipe.cursomc.repositories.EnderecoRepository;
 import com.rodrigofelipe.cursomc.repositories.EstadoRepository;
 import com.rodrigofelipe.cursomc.repositories.ProdutoRepository;
+import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -25,12 +31,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	
+	@Autowired 
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -54,27 +66,34 @@ public class CursomcApplication implements CommandLineRunner {
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		/* categoriaRepository.saveAll salvar os obj no BD */
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlândia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
-		Cidade c3 = new Cidade(null, "Campinas",est2);
-		
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
-		estadoRepository.saveAll(Arrays.asList(est1,est2));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
-		
 
-
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "001001002114", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("33388899", "99998800"));
 		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303","Jardim", "32148899", cli1, c1);
+		Endereco e2 = new Endereco(null, "Sobradinho", "400", "Apto 200","JBrasilia", "32140000", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
 	}
 
