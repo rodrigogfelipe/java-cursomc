a3de8b421f1932508b3ccfd6a7da84f652fbf2e7
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-29 18:49:02 -0300

    Inserindo uma nova categoria com POST

--- src/main/java/com/rodrigofelipe/cursomc/services/CategoriaService.java
+++ src/main/java/com/rodrigofelipe/cursomc/services/CategoriaService.java
@@ -2,13 +2,16 @@ package com.rodrigofelipe.cursomc.services;
 
 import java.util.Optional;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigofelipe.cursomc.domain.Categoria;
 import com.rodrigofelipe.cursomc.repositories.CategoriaRepository;
 import com.rodrigofelipe.cursomc.services.exceptions.ObjectNotFoundException;
 
+@RestController
 @Service
 public class CategoriaService {
 
@@ -20,6 +23,11 @@ public class CategoriaService {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 
 }
