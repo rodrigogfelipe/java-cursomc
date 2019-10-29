c7dcd6674ee112a5d728c26c2cea34922e1dbd0e
Rodrigo Felipe <rodrigogfelipe@hotmail.com>
2019-10-29 00:13:49 -0300

    Pedido. EstadoPagamento e Pagamento

--- src/main/java/com/rodrigofelipe/cursomc/CursomcApplication.java
+++ src/main/java/com/rodrigofelipe/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.rodrigofelipe.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,15 +13,21 @@ import com.rodrigofelipe.cursomc.domain.Cidade;
 import com.rodrigofelipe.cursomc.domain.Cliente;
 import com.rodrigofelipe.cursomc.domain.Endereco;
 import com.rodrigofelipe.cursomc.domain.Estado;
+import com.rodrigofelipe.cursomc.domain.Pagamento;
+import com.rodrigofelipe.cursomc.domain.PagamentoComBoleto;
+import com.rodrigofelipe.cursomc.domain.PagamentoComCartao;
+import com.rodrigofelipe.cursomc.domain.Pedido;
 import com.rodrigofelipe.cursomc.domain.Produto;
+import com.rodrigofelipe.cursomc.domain.enums.EstadoPagamento;
 import com.rodrigofelipe.cursomc.domain.enums.TipoCliente;
 import com.rodrigofelipe.cursomc.repositories.CategoriaRepository;
 import com.rodrigofelipe.cursomc.repositories.CidadeRepository;
 import com.rodrigofelipe.cursomc.repositories.ClienteRepository;
 import com.rodrigofelipe.cursomc.repositories.EnderecoRepository;
 import com.rodrigofelipe.cursomc.repositories.EstadoRepository;
+import com.rodrigofelipe.cursomc.repositories.PagamentoRepository;
+import com.rodrigofelipe.cursomc.repositories.PedidoRepository;
 import com.rodrigofelipe.cursomc.repositories.ProdutoRepository;
-import com.sun.xml.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
@@ -37,12 +44,18 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
-	@Autowired 
+
+	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+
+	@Autowired
+	private PedidoRepository pedidoRepository;
+
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -86,15 +99,28 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "001001002114", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("33388899", "99998800"));
-		
-		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303","Jardim", "32148899", cli1, c1);
-		Endereco e2 = new Endereco(null, "Sobradinho", "400", "Apto 200","JBrasilia", "32140000", cli1, c2);
-		
-		cli1.getEnderecos().addAll(Arrays.asList(e1,e2));
-		
+
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "32148899", cli1, c1);
+		Endereco e2 = new Endereco(null, "Sobradinho", "400", "Apto 200", "JBrasilia", "32140000", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
 
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2)); 
+		 
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6); ped1.setPagamento(pagto1);    
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, 
+				sdf.parse("20/10/2017 00:00"), null); ped2.setPagamento(pagto2); 
+		 
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2)); 
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2)); 
 	}
 
 }
